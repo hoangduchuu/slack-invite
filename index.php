499fb7bd51811d26658bc6adc221d64fa5bf0a4f
Huu Hoang <hoangduchuuvn@gmail.com>
2017-08-30 17:37:15 +0700

    iniza

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Android groups</title>
+</head>
+<body>
+	<form action="return.php" method="POST">
+		<input type="text" placeholder="your email ne" name="email"> <br />
+		<input type="submit" name="submit" id="">
+				
+	</form>
+</body>
+</html>
+
+<?php 
+
+ ?>
---
+++ return.php
@@ -0,0 +1,51 @@
+
+
+<?php 
+
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Android Group</title>
+</head>
+<body>
+	
+</body>
+</html>';
+
+
+$base_url = "https://slack.com/api/users.admin.invite?token=YOUR_TOKEN_HERE&email=";
+$mail = "";
+$mail = $_POST["email"];
+$endUrl = $base_url . $mail;
+
+if (strlen($mail) < 1) {
+	$mail = 'Please input your email';
+}
+
+echo '<b> Hello: </b> '. '<font color=red>' . $mail . '</font>';
+
+// echo '<br />';
+
+// echo '<b> Request url: </b>' . '<font color=red>' . $endUrl . '</font>';
+ // echo '<b> Request url: </b>' . '<font color=red>' . 'hide :P' . '</font>';
+
+
+
+$json = file_get_contents($endUrl);
+$obj = json_decode($json);
+echo '<br />';
+
+$ok = $obj->ok;
+
+echo "<b> Status: </b>";
+if ($ok	 == true) {
+	echo '<font color=red>All done,  Please check your email :) </font>';
+}else {
+	echo '<font color=red>Some thing was wrong , scheck <font color=black><b> Status code</b></font> below </font> <br /> ';
+	echo '<b> Status code: </b>';
+	echo  '<font color=red>' . $obj->error . '</font>';
+}
+echo '<br />';
+echo '<a href="index.php"> back</a>';
+ ?>
